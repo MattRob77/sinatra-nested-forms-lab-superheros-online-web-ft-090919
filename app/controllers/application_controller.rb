4dbb8ce96ca8e682d7b3ca43bba5dcfd430f4a00
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-23 12:04:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
-
-
+    
+    get '/' do 
+      erb: super_hero 
+    end 
+    
+    get '/teams' do 
+      @team = Team.new(params[:team])
+      
+      params[:team][:heroes].each do |deets| 
+        SuperHero.new(deets)
+      end 
+      @heroes = Super_Hero.all 
+    end 
 end
